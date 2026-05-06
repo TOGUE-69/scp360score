8d0b8c99a636cce1b71b54ab0124d8c30e4e1db2
TOGUE-69 <ansymero77@gmail.com>
2026-05-05 20:22:01 -0500

    Update matches.js

--- api/matches.js
+++ api/matches.js
@@ -1,18 +1,17 @@
 export default async function handler(req, res) {
-  const { date } = req.query;
-  const url = `https://free-api-live-football-data.p.rapidapi.com/football-get-matches-by-date?date=${date || '2024-05-03'}`;
-
+  const date = req.query.date;
+  const URL = `https://free-api-live-football-data.p.rapidapi.com/football-get-matches-by-date?date=${date}`;
   try {
-    const response = await fetch(url, {
+    const response = await fetch(URL, {
       method: 'GET',
       headers: {
-        'x-rapidapi-key': process.env.RAPID_KEY,
+        'x-rapidapi-key': 'dd4bf9bdbdmsh73f45643f1efaa7p1a8174jsna126a7e533a0',
         'x-rapidapi-host': 'free-api-live-football-data.p.rapidapi.com'
       }
     });
     const data = await response.json();
     res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Erreur API" });
+  } catch(e) {
+    res.status(500).json({ error: e.message });
   }
 }
